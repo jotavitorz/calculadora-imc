80bc719ea172f9eaa5a56e3fea19a06a39d17e5c
jotavitorz <joaovitorxz812@gmail.com>
2025-09-23 21:33:48 -0300

    style: finalizar estilos do projeto e atualizar index/script

--- script.js
+++ script.js
@@ -6,20 +6,21 @@ function calcularIMC(e) {
     const altura = document.getElementById("altura").value;
     const resultado = document.getElementById("result");
 
+
     const imc = Number(peso) / (Number(altura) * Number(altura));
 
     if(imc < 17){
-        resultado.innerHTML = `<p>Seu resultado: ${imc.toFixed(2)}</p>` + "<span id='imc-extreme'> Cuidado você está muito abaixo do peso! </span>";
+        resultado.innerHTML = `<p>Seu resultado: ${imc.toFixed(2)}</p>` + "<span class='imc-extreme'> Cuidado você está muito abaixo do peso! </span>";
     } else if(imc >= 17 && imc <= 18.49){
-        resultado.innerHTML = `<p>Seu resultado: ${imc.toFixed(2)}</p>` + "<span id='imc-alert'> Você está Abaixo do peso! </span>";
+        resultado.innerHTML = `<p>Seu resultado: ${imc.toFixed(2)}</p>` + "<span class='imc-alert'> Você está Abaixo do peso! </span>";
     }else if(imc >= 18.50 && imc < 24.99){
-        resultado.innerHTML = `<p>Seu resultado: ${imc.toFixed(2)}</p>` + "<span id='imc-ideal'> Você está no peso ideal! </span>";
-    }else if(imc > 25 && imc <= 29.99){
-        resultado.innerHTML = `<p>Seu resultado: ${imc.toFixed(2)}</p>` + "<span id='imc-alert'> Você está acima do peso!e </span>";
+        resultado.innerHTML = `<p>Seu resultado: ${imc.toFixed(2)}</p>` + "<span class='imc-ideal'> Você está no peso ideal! </span>";
+    }else if(imc => 25 && imc <= 29.99){
+        resultado.innerHTML = `<p>Seu resultado: ${imc.toFixed(2)}</p>` + "<span class='imc-alert'> Você está acima do peso! </span>";
     }else if(imc >= 30){
-        resultado.innerHTML = `<p>Seu resultado: ${imc.toFixed(2)}</p>` + "<span id='imc-extreme'> Cuidado Obesidade! </span>";
+        resultado.innerHTML = `<p>Seu resultado: ${imc.toFixed(2)}</p>` + "<span class='imc-extreme'> Cuidado Obesidade! </span>";
     }
 
-    document.getElementById("peso") = "";
-    document.getElementById("altura") = "";
+    document.getElementById("peso").value = "";
+    document.getElementById("altura").value = "";
 }
